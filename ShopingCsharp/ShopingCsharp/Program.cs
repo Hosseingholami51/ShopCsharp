42a89bc36d174dcb0ef56c3ba632802b092c570f
hosseingholami <hosseingholami76@outlook.com>
2018-04-06 13:17:42 +0430

    تغییر کامنت

--- ShopingCsharp/ShopingCsharp/Program.cs
+++ ShopingCsharp/ShopingCsharp/Program.cs
@@ -9,7 +9,7 @@ namespace ShopingCsharp
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application. tset
         /// </summary>
         [STAThread]
         static void Main()
